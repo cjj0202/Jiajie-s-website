9928306d7e68bb9196f087d3b9712c815ec07e7e
cjj0202 <chenjiajie@live.cn>
2014-01-19 22:35:42 -0500

    fix login on home page

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,6 +1,6 @@
 class GalleriesController < ApplicationController
 
-  http_basic_authenticate_with name: "cjj", password: "cjj", except: [:index, :show]
+  http_basic_authenticate_with name: "cjj", password: "cjj", except: [:index, :show, :home]
 
   def create
     @gallery = Gallery.create( params[:gallery] )
