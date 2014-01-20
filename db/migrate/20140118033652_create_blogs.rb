a439e5d2a2cbf54d04652c4604b6a0f6a682e811
cjj0202 <chenjiajie@live.cn>
2014-01-19 21:51:47 -0500

    update blog db migrate for heroku

--- db/migrate/20140118033652_create_blogs.rb
+++ db/migrate/20140118033652_create_blogs.rb
@@ -1,12 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
-    def change
       create_table :blogs do |t|
         t.string :blog_title
         t.text :blog_text
 
         t.timestamps
       end
-    end
   end
 end
