c549b971b7312e689bf7d11e50b0b1ea3904865d
cjj0202 <chenjiajie@live.cn>
2014-01-22 00:15:10 -0500

    some desparate code changes to try fix heroku

--- db/migrate/20140122042336_create_twitter_searches.rb
+++
@@ -1,9 +0,0 @@
-class CreateTwitterSearches < ActiveRecord::Migration
-  def change
-      create_table :twitter_searches do |t|
-      t.string :search_text
-
-      t.timestamps
-    end
-  end
-end
