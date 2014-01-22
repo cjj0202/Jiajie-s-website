b998c6a0e062f60e8c2a52d68244f29f03c4aa30
cjj0202 <chenjiajie@live.cn>
2014-01-22 00:30:30 -0500

    removed some files

--- db/migrate/20140106193432_add_attachment_avatar_to_galleries.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentAvatarToGalleries < ActiveRecord::Migration
-  def self.up
-    add_attachment :galleries, :file
-  end
-
-  def self.down
-    remove_attachment :galleries, :file
-  end
-end
--- db/migrate/20140111043657_add_file_url_to_galleries.rb
+++
@@ -1,5 +0,0 @@
-class AddFileUrlToGalleries < ActiveRecord::Migration
-  def change
-    add_column :galleries, :file_url, :string
-  end
-end
